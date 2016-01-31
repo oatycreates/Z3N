5de887417135b808d5c4ec8276ebab2f39308172
Patrick Ferguson <patrick.ferguson@ancientdawngames.com>
2016-01-31 11:59:31 +1100

    Hide quit buttons on mobile.

---
+++ Assets/Scripts/HideOnMobile.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HideOnMobile : MonoBehaviour {
+
+	// Use this for initialization
+    void Awake ()
+    {
+#if UNITY_IOS || UNITY_IPHONE || UNITY_ANDROID || UNITY_WP8 || UNITY_WP8_1
+        gameObject.SetActive(false);
+#endif
+    }
+
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
